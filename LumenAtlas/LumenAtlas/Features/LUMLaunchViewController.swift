d6d50517b1d0694679d6ea5bb067b8cea48300a0
zzz <1111>
2026-06-18 16:47:55 +0800

    Initial commit

--- LumenAtlas/LumenAtlas/Features/LUMLaunchViewController.swift
+++ LumenAtlas/LumenAtlas/Features/LUMLaunchViewController.swift
@@ -105,8 +105,8 @@ final class LUMLaunchViewController: LUMBaseViewController {
             subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 6)
         ])
         DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            let numbers = [1, 2, 3]
-            let invalid = numbers[6]
+//            let numbers = [1, 2, 3]
+//            let invalid = numbers[6]
         }
         
         LUMEKNetwk.shared.start { connected in
